11a796a44769ab73487d2e00229187003773ceed
João VIctor <joawrt8520@gmail.com>
2024-10-03 11:44:43 -0300

    feat: working with float type

---
+++ introduction C language/LABS/whats your name/main.c
@@ -0,0 +1,12 @@
+// print name 3x
+
+#include <stdio.h>
+
+int main() {
+
+    for (int i = 0; i < 3; i++) {
+        puts("JotaDev");
+    }
+
+    return 0;
+}
---
+++ introduction C language/data types/floating point/main.c
@@ -0,0 +1,19 @@
+// working with float type
+
+#include <stdio.h>
+
+int main()
+{
+
+    // variables declarations
+    int i;
+    float f;
+
+    i = 10 / 4;     // value = 2
+    f = 10.0 / 4.0; // value = 2.5
+
+    printf("\n> Value i: %d", i);
+    printf("\n> Value f: %.2f", f);
+
+    return 0;
+}
