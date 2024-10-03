6fab93478ad8c86ebe882d28a66f0febf2e16eee
João VIctor <joawrt8520@gmail.com>
2024-10-03 18:33:17 -0300

    feat: working with char type

---
+++ introduction C language/data types/characters/main.c
@@ -0,0 +1,16 @@
+// working with char type
+
+#include <stdio.h>
+
+int main()
+{
+
+    // char declarations
+    const char letter = 'A';      // literal
+    const char letter2 = 65;      // ASCII code
+
+    printf("%c\n",letter);
+    printf("%c\n",letter2);
+
+    return 0;
+}
---
+++ introduction C language/data types/operations/operations.c
@@ -0,0 +1,35 @@
+// working with operations in C language
+
+#include <stdio.h>
+
+int main()
+{
+
+    // variables
+    int num1, num2, result;
+    num1 = 10;
+    num2 = 5;
+
+    //multiplication
+    result = num1 * num2;   // result = 50
+
+    // addition
+    result = num1 + num2;   // result = 15
+
+    // subtraction
+    result = num1 - num2;   // result = 5
+
+    // division
+    result = num2 / num1;   // result = 2
+
+    // module
+    result = num1 % num2;   // result = 0
+
+    // operator increment
+    result++;               // result = 1
+
+    // operator decrement
+    result--;               // result = 0;
+
+    return 0;
+}
