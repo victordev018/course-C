8cc031d76a91f149e96a14b3588751fabe706dd1
João VIctor <joawrt8520@gmail.com>
2024-10-04 15:13:00 -0300

    feat: formatted output

---
+++ introduction C language/input_and_output/output/output.c
@@ -0,0 +1,15 @@
+// working with output formatted
+
+#include <stdio.h>
+
+int main()
+{
+    // crating variables
+    int age = 19;
+    float height = 1.69;
+
+    // show data in console
+    printf("\n> My name is Jota, my age is %d and my height is %.2f\n\n", age, height);
+
+    return 0;
+}
