f37d634e1787fb958c3a641a35db4c0edcaecbae
João VIctor <joawrt8520@gmail.com>
2024-10-10 23:52:15 -0300

    feat: created first pointer variable

---
+++ introduction C language/arrays, pointers and strings/pointers/first_pointer/pointer.c
@@ -0,0 +1,15 @@
+
+// pointers are used to store the information about location (address) of any other data
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    // creating integer type pointer variable
+    // will be used to store information about the location of data of type Integer
+
+    int *p;
+
+    return 0;
+}
